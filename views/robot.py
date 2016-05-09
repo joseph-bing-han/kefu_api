99f27bf100b2e24f61b004491d15bdfd522df7a3
houxh <houxuehua49@gmail.com>
2016-05-09 18:01:10 +0800

    bugfix:store_id参数

--- views/robot.py
+++ views/robot.py
@@ -10,17 +10,22 @@ from authorization import require_auth
 app = Blueprint('robot', __name__)
 rpc = xmlrpclib.ServerProxy(config.RPC)
 
+def INVALID_PARAM():
+    e = {"error":"问题为空"}
+    logging.warn("问题为空")
+    return make_response(400, e)
+
 @app.route('/robot/answer', methods=['GET'])
 @require_auth
 def ask_question():
     store_id = request.store_id
     query = request.args.get('question', '')
     if not query:
-        return ""
+        return INVALID_PARAM()
 
     logging.debug("store id:%s, query:%s", store_id, query)
     try:
-        answers = rpc.ask_question(55, query)
+        answers = rpc.ask_question(store_id, query)
         logging.debug("answers:%s", answers)
     except xmlrpclib.ProtocolError as err:
         logging.warning("refresh questions err:%s", err)
